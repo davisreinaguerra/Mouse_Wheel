487c84fe6102014364d17980aadb740600899557
Davis Reina-Guerra <105831652+davisreinaguerra@users.noreply.github.com>
2023-11-13 08:44:28 -0500

    Update treadmill.ino

--- treadmill.ino
+++ treadmill.ino
@@ -9,10 +9,10 @@ alignment align_looming(4);               // D4
 lick_sensor lick(7);                      // D7
                                           // D8 
                                           // D9 ~
-                                         // D10 ~
-#define looming_trig 11                  // D11 ~
-solenoid reward_sol(12);                 // D12
-solenoid puff_sol(13)                    // D13
+                                          // D10 ~
+#define looming_trig 11                   // D11 ~
+solenoid reward_sol(12);                  // D12
+solenoid puff_sol(13);                    // D13
 
 void setup() {
   // put your setup code here, to run once:
