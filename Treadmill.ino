4ac571e15c92cc20ab3573915438a92e829f2673
Davis Reina-Guerra <105831652+davisreinaguerra@users.noreply.github.com>
2024-02-12 16:32:21 -0500

    Add files via upload

--- Treadmill.ino
+++ Treadmill.ino
@@ -7,7 +7,7 @@
 #include "ProbabilityFunctions.h"
 
 //Pin Assignments_____________________
-                                 // D1   unassigneds
+                                 // D1   unassigned
                                  // D2   unassigned
 alignment align_yellow(3);       // D3   Yellow alignment wire
 alignment align_black(4);        // D4   Black alignment wire
@@ -18,8 +18,8 @@ looming looming(8);              // D8   basic trigger using NPN on MatrixPortal
 LED left_LED(9);                 // D9   Green LED wire (left mouse perspective)
 LED right_LED(10);               // D10  Yellow LED wire (right mouse perspective)
 lick_sensor lick(11);            // D11  Wired to cap sensor
-solenoid reward_solenoid(12);    // D12  Yellow Solenoid Wire
-solenoid airpuff_solenoid(13);   // D13  Blue Solenoid Wire
+solenoid reward_solenoid(12);    // D12  
+solenoid airpuff_solenoid(13);   // D13  
 
 
 //Configuration Struct_________________
@@ -33,17 +33,25 @@ struct configuration {
   int puff_duration;
   long intertrial_interval;
   long total_trials;
+  long total_rewards_allowed;
 } configuration;
 
 //_________Variable Initializations_________________
 bool complete = false;
 int trial_number = 0;
-long start_time;
 String coinflip = flip_coin();
+long reward_counter = 0;
+int menu_offset = 0;
+long latency_to_lick;
+long experiment_start_time;
+long trial_start_time;
 
 //_________LCD_Screen_______________________________
 #include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd(0x27,20,4);
+String menu_categories[4] = {"", "", "", ""};
+String updatable_menu[4] = {"", "", "", ""};
+
 
 //_________Modes____________________________________
 //  1: free rewards
@@ -58,13 +66,16 @@ void setup() {
   lcd.init();
   lcd.clear();
   lcd.backlight();
-  lcd.setCursor(0, 0);
-  lcd.print("I'm afraid I cant do that Dave");
+  lcd.setCursor(0, 0); lcd.print("I'm afraid I cant do");
+  lcd.setCursor(0, 1); lcd.print("that Dave...");
+  lcd.setCursor(6, 3); lcd.print("Treadmill_v1.1");
+
+  delay(5000); // hold on title 
 
   // Manual Setup______________________________
-  configuration.mode = 2;
-  configuration.reward_duration = 500;
-  configuration.puff_duration = 500;
+  configuration.mode = 1;
+  configuration.reward_duration = 100;
+  configuration.puff_duration = 200;
   configuration.trial_duration = 600000;
   configuration.delay_between_lick_and_reward = 50;
   configuration.intertrial_interval = 10000;
@@ -77,7 +88,23 @@ void setup() {
   //configuration.reward_duration = read_config();
 
   // Initialize timer for timeout and other time-based calculations
-  start_time = millis();
+
+  // Setting up menu and initilaization stuff based on mode
+  if (configuration.mode == 1) {
+      menu_offset = 12;
+      menu_categories[0] = "mode";
+      menu_categories[1] = "rewards";
+      menu_categories[2] = "elapsed";
+      menu_categories[3] = "latency";
+
+      initialize_menu(menu_offset);
+      updatable_menu[0] = "free";
+      update_menu(menu_offset);
+      right_LED.LED_on();
+      experiment_start_time = millis();
+      trial_start_time = millis();
+      trial_number = 1;
+  }
 }
 
 void loop() {
@@ -85,21 +112,43 @@ void loop() {
 
   switch (configuration.mode) {
     case 1:
-      
+
       // Execute the below code every time the lick sensor reads a lick
       if (lick.is_licked(10)) {                                                  
-        Serial.print("Lick: "); Serial.println(String(millis()-start_time));     // Print to serial port "Lick: Time in ms"
-        align_yellow.align_onset();                                              // Alignment onset to be interpreted as lick
-        delay(configuration.delay_between_lick_and_reward);                      // Wait 
-        reward_solenoid.pulse_valve(configuration.reward_duration);              // Open solenoid to deliver reward
-        align_yellow.align_offset();                                             // Alignment offset to be interpreted as reward delivery
+        
+        // Record Data
+        latency_to_lick = millis() - trial_start_time;                                            // determines latency to lick from LED onset
+        Serial.print("Trial Number: "); Serial.println(String(trial_number));               // print to serial port
+        Serial.print("Lick Latency: "); Serial.println(String(latency_to_lick));            // Print latency to lick to serial port
+        
+        // Open valve and send alignment pulse
+        align_yellow.align_onset();                                                         // Alignment onset to be interpreted as lick
+        delay(configuration.delay_between_lick_and_reward);                                 // Wait 
+        reward_solenoid.pulse_valve(configuration.reward_duration);                         // Open solenoid to deliver reward
+        align_yellow.align_offset();                                                        // Alignment offset to be interpreted as reward delivery
+        
+        // Display data on I2C LCD
+        reward_counter += 1;                                                                // increments up the reward counter
+        updatable_menu[1] = String(reward_counter);                                         // displays number of rewards received in total 
+        updatable_menu[2] = String(millis() - experiment_start_time);                       // displays the time elapsed since the start of the experiment
+        updatable_menu[3] = String(latency_to_lick);                                        // displays latency to lick
+        update_menu(menu_offset);                                                           // updates menu
+        
+        // Reset for next trial
+        right_LED.LED_off();
+        delay(configuration.intertrial_interval);
+        right_LED.LED_on();
+        trial_start_time = millis();
+        trial_number += 1;     
       }
 
       // concludes trial if 
-      if ((millis()-start_time) > configuration.trial_duration) {
+      if (trial_number > configuration.total_trials) {
         complete == true;
       }
       break;
+    
+    
     case 2:
       // Code 
       trial_number++;
@@ -169,4 +218,43 @@ void loop() {
 
 }
 
+// Custom Functions
+void initialize_menu(int menu_line) {
+  lcd.clear();
 
+  // first line - Sound
+  lcd.setCursor(menu_line, 0);
+  lcd.print(menu_categories[0]);
+
+  // second line - Loudness
+  lcd.setCursor(menu_line, 1);
+  lcd.print(menu_categories[1]);
+
+  // third line - Delay
+  lcd.setCursor(menu_line, 2);
+  lcd.print(menu_categories[2]);
+
+  // fourth line - Duration
+  lcd.setCursor(menu_line, 3);
+  lcd.print(menu_categories[3]);
+}
+
+void update_menu(int menu_line) {
+  initialize_menu(menu_line);
+
+  // first line - Sound
+  lcd.setCursor(0, 0);
+  lcd.print(updatable_menu[0]);
+
+  // second line - Loudness
+  lcd.setCursor(0, 1);
+  lcd.print(updatable_menu[1]);
+
+  // third line - Delay
+  lcd.setCursor(0, 2);
+  lcd.print(updatable_menu[2]);
+
+  // fourth line - Duration
+  lcd.setCursor(0, 3);
+  lcd.print(updatable_menu[3]);
+}
