155894c6d5a4ea188796821c4b59109bde96f1e5
Davis Reina-Guerra <105831652+davisreinaguerra@users.noreply.github.com>
2024-02-23 08:09:56 -0500

    Create Sound_Threat.ino

---
+++ Sound_Threat.ino
@@ -0,0 +1,187 @@
+// Libraries
+#include <SPI.h>
+#include <Adafruit_VS1053.h>
+#include <SD.h>
+#include <LiquidCrystal_I2C.h>
+
+// Standard definitions for Adafruit MusicMaker
+#define CLK 13                // SPI Clock, shared with SD card
+#define MISO 12               // Input data, from VS1053/SD card
+#define MOSI 11               // Output data, to VS1053/SD card
+#define SHIELD_RESET  -1      // VS1053 reset pin (unused!)
+#define SHIELD_CS     7       // VS1053 chip select pin (output)
+#define SHIELD_DCS    6       // VS1053 Data/command select pin (output)
+#define CARDCS 4              // Card chip select pin
+#define DREQ 3                // VS1053 Data request, ideally an Interrupt pin
+
+// Trigger
+#define trig_pin 9              // Try to use D13 as the trigger pin for all projects, as it is right next to a GND
+
+// MENU Under construction
+//#define MENU_up_button 2
+//#define MENU_down_button 3
+
+// LOUDNESS Under construction
+//#define LOUDNESS_up_button 4
+//#define LOUDNESS_down_button 5
+
+// DELAY Under construction
+//#define DELAY_up_button 6
+//#define DELAY_down_button 7
+
+// Variable initializations/configurations
+bool trig_state;
+long timer_updating;
+long timer_start;
+
+// crescendo
+uint8_t crescendo_start_vol = 80;
+uint8_t crescendo_end_vol = 40;
+long crescendo_delay = 5;
+
+// I2C Liquid Crystal Display
+LiquidCrystal_I2C lcd(0x27,20,4);
+String menu_categories[4] = {"sound","loudness","delay","duration"};
+String updatable_menu[4] = {"", "", "", ""};
+
+// musicPlayer object
+Adafruit_VS1053_FilePlayer musicPlayer = Adafruit_VS1053_FilePlayer(SHIELD_RESET, SHIELD_CS, SHIELD_DCS, DREQ, CARDCS);
+
+// Manually decide sound name
+String sound_filename = "sweep";
+
+void setup() {
+  Serial.begin(9600);
+
+  // Set up trigger
+  pinMode(trig_pin, INPUT);
+
+  // Initialize LCD screen
+  lcd.init();
+  lcd.clear();
+  lcd.backlight();
+  
+  // Check for Connections
+  if (!musicPlayer.begin()) {while (1);} print_on_line(1, "Music Player Found");
+  if (!SD.begin(CARDCS)) {while (1);} print_on_line(2, "SD Card Found");
+
+  delay(1000);
+
+  // Initialize Menu
+  initialize_menu();
+  updatable_menu[0] = String(sound_filename);
+  update_menu();
+  
+  // Set up interrupt
+  musicPlayer.useInterrupt(VS1053_FILEPLAYER_PIN_INT);  // DREQ int
+
+  // Show the available files
+  printDirectory(SD.open("/"), 0);
+  
+  for(uint8_t i = crescendo_start_vol; i > crescendo_end_vol; i--) {
+    musicPlayer.setVolume(i, i);
+    musicPlayer.startPlayingFile("/white_~1.wav");
+    delay(crescendo_delay);
+  }
+
+  musicPlayer.setVolume(90, 90);
+
+}
+
+
+
+void loop() {
+
+  //musicPlayer.playFullFile("/white_~3.wav");
+  //delay(3000);
+
+  trig_state = digitalRead(trig_pin);
+  Serial.println(trig_state);
+
+  switch (trig_state) {
+    case LOW:
+      Serial.println("Waiting...");
+      break;
+    case HIGH:
+      Serial.println("Playing...");
+      musicPlayer.playFullFile("/sweep_~1.wav");
+      break;
+  }
+
+  delay(50);
+
+}
+
+// Function Declarations_________________________________
+
+
+void print_on_line(int line, String message) {
+  lcd.setCursor(0, line-1);
+  lcd.print(message);
+}
+
+void update_menu() {
+  initialize_menu();
+
+  // first line - Sound
+  lcd.setCursor(0, 0);
+  lcd.print(updatable_menu[0]);
+
+  // second line - Loudness
+  lcd.setCursor(0, 1);
+  lcd.print(updatable_menu[1]);
+
+  // third line - Delay
+  lcd.setCursor(0, 2);
+  lcd.print(updatable_menu[2]);
+
+  // fourth line - Duration
+  lcd.setCursor(0, 3);
+  lcd.print(updatable_menu[3]);
+}
+
+
+void initialize_menu() {
+  lcd.clear();
+
+  // first line - Sound
+  lcd.setCursor(12, 0);
+  lcd.print(menu_categories[0]);
+
+  // second line - Loudness
+  lcd.setCursor(12, 1);
+  lcd.print(menu_categories[1]);
+
+  // third line - Delay
+  lcd.setCursor(12, 2);
+  lcd.print(menu_categories[2]);
+
+  // fourth line - Duration
+  lcd.setCursor(12, 3);
+  lcd.print(menu_categories[3]);
+}
+
+/// File listing helper
+void printDirectory(File dir, int numTabs) {
+   while(true) {
+     
+     File entry =  dir.openNextFile();
+     if (! entry) {
+       // no more files
+       break;
+     }
+     for (uint8_t i=0; i<numTabs; i++) {
+       Serial.print('\t');
+     }
+     Serial.print(entry.name());
+     if (entry.isDirectory()) {
+       Serial.println("/");
+       printDirectory(entry, numTabs+1);
+     } else {
+       // files have sizes, directories do not
+       Serial.print("\t\t");
+       Serial.println(entry.size(), DEC);
+     }
+     entry.close();
+   }
+}
